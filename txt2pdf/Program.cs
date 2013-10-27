549ee27a06a2e42df651739fe8834d973b1a0d77
MilosZlatkovic <milos_zlatkovic@ymail.com>
2013-10-27 10:59:09 +0100

    work is done

---
+++ txt2pdf/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace txt2pdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(args[0], Encoding.GetEncoding("windows-1250"));
+
+            Document doc = new Document(iTextSharp.text.PageSize.A4, 50, 20, 20, 20);
+            PdfWriter pdfwr = PdfWriter.GetInstance(doc,
+                                                    new FileStream(args[0].ToString().Substring(0, args[0].ToString().Length - 4) + ".pdf",
+                                                    FileMode.Create));
+            doc.Open();
+
+            // Create my base font
+            BaseFont base_font = BaseFont.CreateFont("consola.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            // Create my font
+            Font my_font = new Font(base_font, 11, iTextSharp.text.Font.NORMAL, BaseColor.BLACK);
+
+            Paragraph paragraph;
+            foreach (string line in lines)
+            {
+                paragraph = new Paragraph(line, my_font);
+                doc.Add(paragraph);
+            }
+
+            doc.Close();
+
+            //System.Console.WriteLine("Done! \n");
+            //Console.WriteLine("Press any key to exit.");
+            //System.Console.ReadKey();  
+        }
+    }
+}
